d436421454ef66ad8796dbc3ec79ac740f6c4922
Gurtej Kanwar <gurtej@mit.edu>
2020-07-13 22:25:02 -0400

    Adds baby echo command to haskell term

--- user/terminal.hs
+++ user/terminal.hs
@@ -1,6 +1,8 @@
+import Data.List
+
 -- Sketch of a terminal user-space application
 
-newtype Token = Token String
+newtype Token = Token {str :: String}
   deriving (Eq,Show)
 
 tokenize :: String -> [Token]
@@ -9,9 +11,15 @@ tokenize s = map Token (words s) -- FORNOW
 -- TODO: Use state monad to pass around an environ
 -- resolveVars :: Environ -> Token -> Token
 
+echo :: [Token] -> IO ()
+echo toks = putStrLn $ concat (intersperse " " (map str toks))
+
 handleCmd :: [Token] -> IO Bool
 handleCmd [] = return False
 handleCmd ((Token "exit"):args) = return True
+handleCmd ((Token "echo"):args) = do
+  echo args
+  return False
 handleCmd ((Token s):args) = do
   putStrLn ("unrecognized cmd " ++ s)
   return False
