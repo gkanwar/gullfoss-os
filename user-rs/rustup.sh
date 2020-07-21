6053fd7ee3d7de73a4e448005e86b6ec259a5cc6
Gurtej Kanwar <gurtej@mit.edu>
2020-07-20 20:25:26 -0400

    Removes is-like-osx and adds check for cross compiler
    Finally, rust targets build on BOTH linux and OSX.

--- user-rs/rustup.sh
+++ user-rs/rustup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+# Need the nightly rustc to use some language features
 rustup toolchain install nightly
 rustup update nightly --force # get the very latest for now (7/17 build relevant)
 rustup default nightly
+
+# Need rust source to cross-compiler the core library
 rustup component add rust-src
 
+# Need a gcc cross compiler available at ../../opt/cross64
+if [[ -d "../../opt/cross64" ]]; then
+    echo "Found 64-bit cross compiler dir, we are good to go!"
+else
+    echo "Need 64-bit cross compiler (actually cross-linker)," \
+         "please install in ../../opt/cross64"
+    exit 1
+fi
