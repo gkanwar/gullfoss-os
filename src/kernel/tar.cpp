6684e0ea0f0bdfb12c6ee0e21f53c927fbcad184
Gurtej Kanwar <gurtej@mit.edu>
2020-07-23 21:53:53 -0400

    Cleans out some useless prints

--- src/kernel/tar.cpp
+++ src/kernel/tar.cpp
@@ -6,7 +6,6 @@
 using FileHeader = Tarball::FileHeader;
 
 static const FileHeader* step_forward(const FileHeader* header, void* end_buffer) {
-  debug::serial_printf("step_forward %p (end %p)\n", header, end_buffer);
   lsize_t size = std::strtol((const char*)header->size, nullptr, 8);
   lsize_t next_ptr = (lsize_t)(header+1);
   next_ptr += size;
@@ -17,7 +16,6 @@ static const FileHeader* step_forward(const FileHeader* header, void* end_buffer
   if (header >= end_buffer) {
     return nullptr;
   }
-  debug::serial_printf("step_forward result = %p\n", header);
   return header;
 }
 
@@ -26,15 +24,12 @@ tar_file_t Tarball::find_file(const char* name) {
   const FileHeader* header = (const FileHeader*)buffer;
   void* end_buffer = (void*)((uint8_t*)buffer + size);
   do {
-    debug::serial_printf("Checking entry with filename %s\n", header->name);
     if (std::strcmp((const char*)header->name, name) == 0) {
       return {
         .buffer = (uint8_t*)(header + 1), // data is just past (padded) header
         .size = (lsize_t)std::strtol((const char*)header->size, nullptr, 8)
       };
     }
-    debug::serial_printf("Skipping forward (size = %llu)\n",
-                         std::strtol((const char*)header->size, nullptr, 8));
     header = step_forward(header, end_buffer);
   } while(header);
   return { .buffer = nullptr, .size = 0 };
