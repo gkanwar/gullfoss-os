751d5df5fae435b352ae60bc45745b84350b09e6
Gurtej Kanwar <gurtej@mit.edu>
2020-07-24 00:25:30 -0400

    Adds extra args to test_kernel_serial.sh

--- test_kernel_serial.sh
+++ test_kernel_serial.sh
@@ -2,7 +2,7 @@
 
 TARGET=$1
 if [[ "x${TARGET}" == "x" ]]; then
-    echo "Usage: $0 <target>"
+    echo "Usage: $0 <target> [extra args]"
     exit 1
 fi
-qemu-system-x86_64 -no-reboot -serial stdio -cdrom bin/${TARGET}/gullfoss.iso
+qemu-system-x86_64 -no-reboot -serial stdio -cdrom bin/${TARGET}/gullfoss.iso ${@:2}
